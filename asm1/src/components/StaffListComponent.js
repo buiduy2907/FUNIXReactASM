5f1ebebfc1c762737d2c9d52e7355cc2bbf30a14
BUIDUY <buiduy.us@gmail.com>
2022-01-19 20:40:40 +0700

    UI0.1

--- asm1/src/components/StaffListComponent.js
+++ asm1/src/components/StaffListComponent.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {CardText,Card,CardBody,CardTitle} from 'reactstrap'
+import {CardText,Card,CardBody,CardTitle,CardImg} from 'reactstrap'
 import dateFormat from 'dateformat'
 
 
@@ -9,7 +9,7 @@ class StaffList extends Component{
 
         this.state={
             selectedStaff:null,
-            colSet:'col-lg-4'
+            colSet:'col-xs-12 col-sm-6 col-md-4 col-lg-4'
         }
     }
     onStaffSelect(staff){
@@ -20,17 +20,17 @@ class StaffList extends Component{
     }
     onSet2(){
         this.setState({
-            colSet: 'col-lg-6'
+            colSet: 'col-xs-12 col-sm-6 col-md-6 col-lg-6'
         })
     }
     onSet3(){
         this.setState({
-            colSet: 'col-lg-4'
+            colSet: 'col-xs-12 col-sm-4 col-md-4 col-lg-4'
         })
     }
     onSet6(){
         this.setState({
-            colSet: 'col-lg-2'
+            colSet: 'col-xs-12 col-sm-2 col-md-2 col-lg-2'
         })
     }
 
@@ -38,14 +38,16 @@ class StaffList extends Component{
         if(staff!=null){
             const department = staff.department.name;
             return(
-                <div className='my-1'>
-                    <h2>Họ và tên: {staff.name}</h2>
-                    <div>Ngày sinh: {dateFormat(staff.doB,"dd/mm/yyyy")}</div>
-                    <div>Ngày vào công ty: {dateFormat(staff.startDate,"dd/mm/yyyy")}</div>
-                    <div>Phòng ban{department}</div>
-                    <div>Số ngày nghỉ còn lại: {staff.annualLeave}</div>
-                    <div>Số ngày đã làm thêm: {staff.overTime}</div>
-                </div>
+                <Card className='my-1'>
+                    <CardTitle className='h4'>Họ và tên: {staff.name}</CardTitle>
+                    <CardBody>
+                        <CardTitle>Ngày sinh: {dateFormat(staff.doB,"dd/mm/yyyy")}</CardTitle>
+                        <CardText>Ngày vào công ty: {dateFormat(staff.startDate,"dd/mm/yyyy")}</CardText>
+                        <CardText>Phòng ban{department}</CardText>
+                        <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+                        <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+                    </CardBody>
+                </Card>
             )
         }
         else{
@@ -70,6 +72,7 @@ class StaffList extends Component{
                     {liststaff}
                 </div>
                 <div className='my-2'>
+                    <h5>Hiển thị trang:</h5>
                     <input type="button" value={2} onClick={()=>this.onSet2()}></input>
                     <input type="button" value={3} onClick={()=>this.onSet3()}></input>
                     <input type="button" value={6} onClick={()=>this.onSet6()}></input>

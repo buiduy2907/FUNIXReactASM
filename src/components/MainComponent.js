03cd9467bdf9ce07d69321745c6c79bcf1fd45b9
BUIDUY <buiduy.us@gmail.com>
2022-01-23 18:01:23 +0700

    LAB05

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React,{Component} from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from "./MenuComponent";
 import DishDetail from './DishDetailComponent';
 import {DISHES} from "../shared/dishes";
+import Header from './HeaderComponet';
+import Footer from './FooterComponent';
 
 class Main extends Component{
   
@@ -21,14 +22,11 @@ class Main extends Component{
   render(){
   return ( 
     <div>
-      <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Rostrate Confusion</NavbarBrand>
-          </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} 
+    <Header />
+    <Menu dishes={this.state.dishes}
       onClick ={(dishId)=>this.onDishSelect(dishId)}/>
-      <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]} />
+    <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]} />
+    <Footer/>
     </div>
   )
   }
